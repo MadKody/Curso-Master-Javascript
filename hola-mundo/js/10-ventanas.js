4198691d4476fca6577237a87482c5af549adef1
MadKody <40124561+MadKody@users.noreply.github.com>
2018-06-13 23:55:52 +0200

    Ventanas

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//ALERTA
+
+//alert("Esta es mi alerta...ehm...Hola mundo Vol.2");
+
+//Confirmacion
+var mi_resultado = confirm("Estas seguro de querer continuar?")
+console.log(mi_resultado);
+
+//Ingreso de informacion
+
+var resultado = prompt("Que edad tienes?", 18);
+console.log(typeof(parseInt (resultado)));
